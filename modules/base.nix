381770f6dbd05eeee86a73786b5ce6fee96718bd
griffi-gh <prasol258@gmail.com>
2024-11-20 18:17:37 +0100

    oops...

--- modules/base.nix
+++ modules/base.nix
@@ -12,11 +12,14 @@
   i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "UTC";
 
-  programs.git.enable = true;
-  programs.command-not-found.enable = false;
-  programs.mosh = {
-    enable = true;
-    openFirewall = true;
+  programs= {
+    fish.enable = true;
+    git.enable = true;
+    command-not-found.enable = false;
+    mosh = {
+      enable = true;
+      openFirewall = true;
+    };
   };
   environment.systemPackages = with pkgs; [
     htop
