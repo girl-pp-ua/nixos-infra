bab944f085e98a49b0372c998e9d2c2396bedfd2
griffi-gh <prasol258@gmail.com>
2025-01-03 20:52:55 +0100

    add new ssh key

--- modules/base/users.nix
+++ modules/base/users.nix
@@ -12,6 +12,9 @@
       openssh.authorizedKeys.keys = [
         # dell-pc
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeD5o9/2cpH7HaP4PldZYKgJnltFX9XlC5bvcxpOivq user@dell-pc"
+        # fw13
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB23Z5khaSPYhYX1AZFZIVKwjXaDZbGQFVxzPPWa6f4r user@fw13"
+        # a52s termux
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFWfqwIKwAjzx3WbJc0pYcJllV0BIpJSf9vm3lM8Ubl u0_a371@localhost"
       ];
     };
