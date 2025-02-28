b4b7aa10ed07ba418e1db8d684f59dcfffa2ede9
griffi-gh <prasol258@gmail.com>
2025-02-28 18:38:53 +0100

    update caddy hash

--- modules/base/services/caddy.nix
+++ modules/base/services/caddy.nix
@@ -12,7 +12,7 @@ let cfg = config.cfg; in {
         plugins = [
           "github.com/mholt/caddy-webdav@v0.0.0-20241008162340-42168ba04c9d"
         ];
-        hash = "sha256-ZknEK1y3+0D9A72C0T7wD3P6utFsdJld+2hRpG0WRG8=";
+        hash = "sha256-WxPPQdRIJNs/Cx+IP4i3jefFqksTxCRPPtVgDPEdSLM=";
       };
       enableReload = true;
       adapter = "caddyfile";
