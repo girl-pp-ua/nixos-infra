545ec3d9c442c2f25effc2fbbe301eb6ee925af1
griffi-gh <prasol258@gmail.com>
2025-09-21 19:29:55 +0200

    update hydroxide stuff

--- modules/services/hydroxide/default.nix
+++ modules/services/hydroxide/default.nix
@@ -11,11 +11,24 @@
   };
 
   config = lib.mkIf config.cfg.services.hydroxide.enable {
-    services.hydroxide = {
-      enable = true;
-      authfile = config.sops.secrets."hydroxide/auth".path;
-      carddav.enable = false; # requires proxy setup
-    };
+    services.hydroxide =
+      let
+        host = "100.64.0.2";
+      in
+      {
+        enable = true;
+        debug = true;
+        authfile = config.sops.secrets."hydroxide/auth".path;
+        carddav.enable = false; # requires proxy setup
+        imap = {
+          inherit host;
+          port = 1143;
+        };
+        smtp = {
+          inherit host;
+          port = 1025;
+        };
+      };
     sops.secrets."hydroxide/auth" = { };
   };
 }
