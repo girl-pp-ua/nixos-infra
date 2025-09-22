e4d2e949a42998561b4aa7bb04f1edfc71d66e0e
griffi-gh <prasol258@gmail.com>
2025-09-22 13:00:07 +0200

    imrprove dns config

--- modules/base/networking/dns.nix
+++ modules/base/networking/dns.nix
@@ -1,12 +1,24 @@
-{ ... }:
+{ lib, ... }:
 {
   services.resolved = {
     enable = true;
-    dnssec = "false";
-    dnsovertls = "false";
+    dnssec = "allow-downgrade";
+    dnsovertls = "opportunistic";
     extraConfig = ''
       [Resolve]
-      DNS=208.67.220.2 208.67.222.2 9.9.9.10 149.112.112.10
+      DNS=${
+        lib.concatStringsSep " " [
+          "2620:0:ccd::2#sandbox.opendns.com"
+          "2620:0:ccc::2#sandbox.opendns.com"
+          "2620:fe::10#dns10.quad9.net"
+          "2620:fe::fe:10#dns10.quad9.net"
+
+          "208.67.220.2#sandbox.opendns.com"
+          "208.67.222.2#sandbox.opendns.com"
+          "9.9.9.10#dns10.quad9.net"
+          "149.112.112.10#dns10.quad9.net"
+        ]
+      }
       Domains=~.
     '';
   };
@@ -22,6 +34,8 @@
     matchConfig.Name = "dummy0";
     networkConfig = {
       DNS = "127.0.0.1";
+      DNSOverTLS = false;
+      DNSSEC = false;
       Domains = [ "~nix-infra" ];
       LinkLocalAddressing = "ipv4";
       DHCP = "no";
