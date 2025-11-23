889bdb6a37d013e5fa269f08217669bba27487ae
griffi-gh <prasol258@gmail.com>
2025-11-23 21:14:08 +0100

    Update supervisor-host.nix

--- modules/services/supervisor-host.nix
+++ modules/services/supervisor-host.nix
@@ -25,86 +25,83 @@ in
     };
   };
 
-  config =
-    (lib.mkIf cfg.enable {
-      # Enable virtualization
-      virtualisation.libvirtd = {
-        enable = true;
-        qemu = {
-          package = pkgs.qemu_kvm;
-          ovmf = {
-            enable = true;
-            packages = [ pkgs.OVMFFull.fd ];
-          };
-          swtpm.enable = true;
-          runAsRoot = false;
+  config = lib.mkIf cfg.enable {
+    # Enable virtualization
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;
+        runAsRoot = false;
+      };
+    };
+
+    # Enable Cockpit
+    services.cockpit = {
+      enable = true;
+      inherit (cfg.cockpit) port;
+      showBanner = false;
+      allowed-origins = [
+        "http://${cfg.cockpit.intraDomain}"
+      ];
+      settings = {
+        WebService = {
+          AllowUnencrypted = true;
+          ForwardedForHeader = "X-Forwarded-For";
+          ProtocolHeader = "X-Forwarded-Proto";
         };
       };
+    };
 
-      # Enable Cockpit
-      services.cockpit = {
-        enable = true;
-        inherit (cfg.cockpit) port;
-        showBanner = false;
-        settings = {
-          WebService = {
-            AllowUnencrypted = true;
-            ForwardedForHeader = "X-Forwarded-For";
-            ProtocolHeader = "X-Forwarded-Proto";
-            Origins = "http://${cfg.cockpit.intraDomain}";
-          };
-        };
-      };
+    # environment.systemPackages = with pkgs; [
+    #   cockpit-machines
+    # ];
 
-      environment.systemPackages = with pkgs; [
-        cockpit-machines
-      ];
+    # Add your user to necessary groups
+    users.users.nixos.extraGroups = [
+      "libvirtd"
+      "kvm"
+      "qemu-libvirtd"
+    ];
 
-      # Add your user to necessary groups
-      users.users.nixos.extraGroups = [
-        "libvirtd"
-        "kvm"
-        "qemu-libvirtd"
-      ];
-
-      # Enable nested virtualization (optional but recommended)
-      boot.extraModprobeConfig = ''
-        options kvm_amd nested=1
-      '';
-
-      services.caddy.virtualHosts = {
-        "http://${cfg.cockpit.intraDomain}" = {
-          extraConfig = ''
-            import encode
-            reverse_proxy http://127.0.0.1:${toString cfg.cockpit.port}
-          '';
-        };
-      };
-
-    })
-    // (lib.mkIf (cfg.enable && cfg.gpuPassthrough) {
-      # Enable IOMMU (AMD)
-      boot.kernelParams = [
-        "amd_iommu=on"
-        "iommu=pt"
-        "video=efifb:off" # Disable EFI framebuffer to free the GPU
-      ];
-
-      # Load VFIO modules early
-      boot.initrd.kernelModules = [
-        "vfio_pci"
-        "vfio"
-        "vfio_iommu_type1"
-      ];
-
+    # Enable nested virtualization (optional but recommended)
+    boot.extraModprobeConfig = ''
+      options kvm_amd nested=1
+    ''
+    +
       # Bind GPU to VFIO at boot
-      boot.extraModprobeConfig = ''
+      (lib.optionalString cfg.gpuPassthrough ''
         options vfio-pci ids=1002:15e7,1002:1637
         softdep amdgpu pre: vfio-pci
         softdep snd_hda_intel pre: vfio-pci
-      '';
+      '');
 
-      # Blacklist AMD GPU drivers on host
-      boot.blacklistedKernelModules = [ "amdgpu" ];
-    });
+    # Enable IOMMU (AMD)
+    boot.kernelParams = lib.optionals cfg.gpuPassthrough [
+      "amd_iommu=on"
+      "iommu=pt"
+      "video=efifb:off" # Disable EFI framebuffer to free the GPU
+    ];
+
+    # Load VFIO modules early
+    boot.initrd.kernelModules = lib.optionals cfg.gpuPassthrough [
+      "vfio_pci"
+      "vfio"
+      "vfio_iommu_type1"
+    ];
+
+    # Blacklist AMD GPU drivers on host
+    boot.blacklistedKernelModules = lib.optionals cfg.gpuPassthrough [
+      "amdgpu"
+    ];
+
+    services.caddy.virtualHosts = {
+      "http://${cfg.cockpit.intraDomain}" = {
+        extraConfig = ''
+          import encode
+          reverse_proxy http://127.0.0.1:${toString cfg.cockpit.port}
+        '';
+      };
+    };
+  };
 }
