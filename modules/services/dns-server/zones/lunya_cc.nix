222cb0f36af5f96a934b6886f02291aa38f8f1c6
griffi-gh <prasol258@gmail.com>
2026-02-13 12:52:31 +0100

    drop unused lunya.cc dns entries

--- modules/services/dns-server/zones/lunya_cc.nix
+++ modules/services/dns-server/zones/lunya_cc.nix
@@ -40,13 +40,11 @@ in
       dell-sv-proxy.CNAME = [ "oci2" ]; # -> reverse proxy
 
       status.CNAME = [ "oci1" ];
-      ntfy.CNAME = [ "oci2" ];
 
-      reddit.CNAME = [ "oci2" ];
-
-      cloud.CNAME = [ "dell-sv-proxy" ];
-      photos.CNAME = [ "dell-sv-proxy" ];
-      docs.CNAME = [ "dell-sv-proxy" ];
+      # ntfy.CNAME = [ "oci2" ];
+      # cloud.CNAME = [ "dell-sv-proxy" ];
+      # photos.CNAME = [ "dell-sv-proxy" ];
+      # docs.CNAME = [ "dell-sv-proxy" ];
     };
   };
 }
