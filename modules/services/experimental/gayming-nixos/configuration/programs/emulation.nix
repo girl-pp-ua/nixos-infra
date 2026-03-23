d12e221296ef6b8d73631d0c362cc4b7a7d26c0e
griffi-gh <prasol258@gmail.com>
2026-03-23 16:25:31 +0100

    Update emulation.nix

--- modules/services/experimental/gayming-nixos/configuration/programs/emulation.nix
+++ modules/services/experimental/gayming-nixos/configuration/programs/emulation.nix
@@ -1,4 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    eden
+    ryubing
+    steam-rom-manager
+  ];
+  gayming.labwc-headless.menuItems = [
+    {
+      label = "[switch] Eden";
+      command = "eden";
+    }
+    {
+      label = "[switch] Ryujinx";
+      command = "ryujinx";
+    }
+    {
+      label = "Steam ROM Manager";
+      command = "steam-rom-manager";
+    }
+  ];
 
 }
