c3813c66627065957f68c7fdb4e5007f0726449e
griffi-gh <prasol258@gmail.com>
2026-07-11 14:37:53 +0200

    switch back to nixos-unstable chan

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
