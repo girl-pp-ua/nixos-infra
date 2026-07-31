8c10526f66dd71ab17036aab892da267f38a3726
griffi-gh <prasol258@gmail.com>
2026-07-31 19:54:01 +0200

    drop a52s key

--- modules/base/users.nix
+++ modules/base/users.nix
@@ -15,8 +15,6 @@
       openssh.authorizedKeys.keys = [
         # fw13
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB23Z5khaSPYhYX1AZFZIVKwjXaDZbGQFVxzPPWa6f4r user@fw13"
-        # a52s termux
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFWfqwIKwAjzx3WbJc0pYcJllV0BIpJSf9vm3lM8Ubl u0_a371@localhost"
       ];
     };
   };
